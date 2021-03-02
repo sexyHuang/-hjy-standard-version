9e3579e34c3b8597662c60b60c14b1b553a10c25
huangjianyong <huangjianyong@tuzhanai.com>
2021-03-02 17:12:13 +0800

    feat: 提示文案修改

--- bin/log.js
+++ bin/log.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 
-const error = (...text) => console.log(chalk.bold.red("[!] Error: ", ...text));
+const error = (...text) => console.log(chalk.bold.red("[!] Error:", ...text));
 const warning = (...text) => console.log(chalk.keyword("orange")(...text));
 
 const info = (...text) => console.log(chalk.cyan(...text));
--- bin/test.js
+++
@@ -1,8 +0,0 @@
-const execa = require("execa");
-
-const test = async () => {
-  const { stdout } = await execa("git", ["ls-remote"]);
-  console.log(stdout);
-};
-
-test();
