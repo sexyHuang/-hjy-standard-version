a72b5ebd8ea79b788f0875edf1848668447328a6
huangjianyong <huangjianyong@tuzhanai.com>
2021-04-07 14:36:14 +0800

    feat: 优化选项

--- bin/index.js
+++ bin/index.js
@@ -7,9 +7,15 @@ const { version } = require("./../package.json");
 const execa = require("execa");
 const configCommand = require("./configCommand");
 const config = require("../config/index.json");
+const standardVersion = require("standard-version");
+const { getConfiguration } = require("standard-version/lib/configuration");
 const { error, info } = require("./log");
 
-const releaseTypes = ["major", "minor", "patch", "first-release"];
+const svConfig = getConfiguration();
+
+const releaseTypes = ["major", "minor", "patch", "first-release", "prerelease"];
+
+const prereleasePrefixes = ["alpha", "beta", "custom"];
 const getNowStr = () => {
   const now = new Date();
   return `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
@@ -27,6 +33,27 @@ const promptList = [
     message: "请选择release类型",
     choices: releaseTypes,
     default: releaseTypes[2],
+  },
+  {
+    type: "list",
+    name: "prerelease",
+    when({ releaseType }) {
+      return releaseType === "prerelease";
+    },
+    message: "请选择prerelease类型",
+    choices: prereleasePrefixes,
+    default: prereleasePrefixes[0],
+  },
+  {
+    type: "input",
+    name: "customPrereleasePrefix",
+    message: "请输入prerelease前缀",
+    when({ prerelease }) {
+      return prerelease === "custom";
+    },
+    validate(input) {
+      return input ? true : "请输入prerelease前缀!";
+    },
   },
   {
     type: "input",
@@ -57,35 +84,41 @@ const isLegalBranch = (currentBranch, ignore) => {
   return new RegExp(config.legalBranch).test(currentBranch);
 };
 
-const standardVersion = join(
-  __dirname,
-  "./../node_modules/.bin/standard-version"
-);
+// const standardVersion = join(
+//   __dirname,
+//   "./../node_modules/.bin/standard-version"
+// );
 
 const tagScript = async () => {
-  const { releaseType, message } = await prompt(promptList);
-
+  let {
+    releaseType,
+    message,
+    prerelease,
+    customPrereleasePrefix,
+  } = await prompt(promptList);
+  let config = { ...svConfig, message: messageTransformer(message) };
   if (releaseType === "first-release")
-    await execa(
-      standardVersion,
-      [
-        "--releaseCommitMessageFormat",
-        messageTransformer(message),
-        "--first-release",
-      ],
-      { stdio: "inherit" }
-    );
-  else
-    await execa(
-      standardVersion,
-      [
-        "-r",
-        releaseType,
-        "--releaseCommitMessageFormat",
-        messageTransformer(message),
-      ],
-      { stdio: "inherit" }
-    );
+    config = {
+      ...config,
+      firstRelease: true,
+    };
+  else if (releaseType === "prerelease") {
+    if (prerelease === "custom") {
+      prerelease = customPrereleasePrefix;
+    }
+    config = {
+      ...config,
+      message,
+      prerelease,
+    };
+  } else {
+    config = {
+      ...config,
+      releaseType,
+    };
+  }
+
+  return standardVersion(config);
 };
 const pushScript = async (currentBranch) => {
   if (!(await isSetRemote())) {
