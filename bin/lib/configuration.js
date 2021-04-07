ecd4ce836f6d3dba2ff36ae35a1202cb7b2472f0
huangjianyong <huangjianyong@tuzhanai.com>
2021-04-07 16:37:36 +0800

    fix(lib/configuration.js): 路径问题

--- bin/lib/configuration.js
+++ bin/lib/configuration.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const findUp = require("find-up");
 const { readFileSync } = require("fs");
-const _config = require("../config/index.json");
+const _config = require("./../../config/index.json");
 const CONFIGURATION_FILES = [".svrc", ".svrc.json"];
 module.exports.getConfiguration = function () {
   const configPath = findUp.sync(CONFIGURATION_FILES);
